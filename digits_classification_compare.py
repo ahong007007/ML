0eb8726ea9bf41af3c2203071514af945bc3cdba
ahong007007 <754944643@qq.com>
2017-06-13 17:53:48 +0800

    Create digits_classification_compare.py

---
+++ digits_classification_compare.py
@@ -0,0 +1,143 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+#python运行版本py3.6
+#依据Recognizing hand-written digits,使用['NB', 'KNN', 'LR', 'RF', 'DT', 'SVM', 'GBDT']等做的多分类器对比
+#当然各个参数没有调试好，SVM怎么只有39.6%的正确率，简直可笑
+# （scikit-learn http://scikit-learn.org/stable/auto_examples/classification/plot_digits_classification.html）
+import sys
+import os
+import time
+from sklearn import metrics,datasets
+import numpy as np
+
+
+
+# Multinomial Naive Bayes Classifier
+def naive_bayes_classifier(train_x, train_y):
+    from sklearn.naive_bayes import MultinomialNB
+    model = MultinomialNB(alpha=0.01)
+    model.fit(train_x, train_y)
+    return model
+
+
+# KNN Classifier
+def knn_classifier(train_x, train_y):
+    from sklearn.neighbors import KNeighborsClassifier
+    model = KNeighborsClassifier()
+    model.fit(train_x, train_y)
+    return model
+
+
+# Logistic Regression Classifier
+def logistic_regression_classifier(train_x, train_y):
+    from sklearn.linear_model import LogisticRegression
+    model = LogisticRegression(penalty='l2')
+    model.fit(train_x, train_y)
+    return model
+
+
+# Random Forest Classifier
+def random_forest_classifier(train_x, train_y):
+    from sklearn.ensemble import RandomForestClassifier
+    model = RandomForestClassifier(n_estimators=8)
+    model.fit(train_x, train_y)
+    return model
+
+
+# Decision Tree Classifier
+def decision_tree_classifier(train_x, train_y):
+    from sklearn import tree
+    model = tree.DecisionTreeClassifier()
+    model.fit(train_x, train_y)
+    return model
+
+
+# GBDT(Gradient Boosting Decision Tree) Classifier
+def gradient_boosting_classifier(train_x, train_y):
+    from sklearn.ensemble import GradientBoostingClassifier
+    model = GradientBoostingClassifier(n_estimators=200)
+    model.fit(train_x, train_y)
+    return model
+
+
+# SVM Classifier
+def svm_classifier(train_x, train_y):
+    from sklearn.svm import SVC
+    model = SVC(kernel='rbf', probability=True)
+    model.fit(train_x, train_y)
+    return model
+
+
+# SVM Classifier using cross validation
+def svm_cross_validation(train_x, train_y):
+    from sklearn.grid_search import GridSearchCV
+    from sklearn.svm import SVC
+    model = SVC(kernel='rbf', probability=True)
+    param_grid = {'C': [1e-3, 1e-2, 1e-1, 1, 10, 100, 1000], 'gamma': [0.001, 0.0001]}
+    grid_search = GridSearchCV(model, param_grid, n_jobs=1, verbose=1)
+    grid_search.fit(train_x, train_y)
+    best_parameters = grid_search.best_estimator_.get_params()
+    for para, val in best_parameters.items():
+        print(para,val)
+    model = SVC(kernel='rbf', C=best_parameters['C'], gamma=best_parameters['gamma'], probability=True)
+    model.fit(train_x, train_y)
+    return model
+
+
+def read_data():
+    # The digits dataset from scikit-learn
+    digits = datasets.load_digits()
+    # To apply a classifier on this data, we need to flatten the image, to
+    # turn the data in a (samples, feature) matrix:
+    n_samples = len(digits.images)
+    data = digits.images.reshape((n_samples, -1))
+    train_x,train_y=data[:int(n_samples / 2)], digits.target[:int(n_samples / 2)]
+    # Now predict the value of the digit on the second half:
+    test_x=data[int(n_samples / 2):]
+    test_y = digits.target[int(n_samples / 2):]
+
+
+    return train_x, train_y, test_x, test_y
+
+
+if __name__ == '__main__':
+    data_file = "mnist.pkl.gz"
+    thresh = 0.5
+    model_save_file = None
+    model_save = {}
+
+    test_classifiers = ['NB', 'KNN', 'LR', 'RF', 'DT', 'SVM', 'GBDT']
+    classifiers = {'NB': naive_bayes_classifier,
+                   'KNN': knn_classifier,
+                   'LR': logistic_regression_classifier,
+                   'RF': random_forest_classifier,
+                   'DT': decision_tree_classifier,
+                   'SVM': svm_classifier,
+                   'SVMCV': svm_cross_validation,
+                   'GBDT': gradient_boosting_classifier
+                   }
+
+    print('reading training and testing data...')
+    train_x, train_y, test_x, test_y = read_data()
+    num_train, num_feat = train_x.shape
+    num_test, num_feat = test_x.shape
+    is_binary_class = (len(np.unique(train_y)) == 2)
+    print('******************** Data Info *********************')
+    print('#training data: %d, #testing_data: %d, dimension: %d' % (num_train, num_test, num_feat))
+
+
+    for classifier in test_classifiers:
+        print('******************* %s ********************' % classifier)
+        start_time = time.time()
+        model = classifiers[classifier](train_x, train_y)
+        print('training took %fs!' % (time.time() - start_time))
+        predict = model.predict(test_x)
+        if model_save_file != None:
+            model_save[classifier] = model
+        if is_binary_class:
+            precision = metrics.precision_score(test_y, predict)
+            recall = metrics.recall_score(test_y, predict)
+            print
+            'precision: %.2f%%, recall: %.2f%%' % (100 * precision, 100 * recall)
+        accuracy = metrics.accuracy_score(test_y, predict)
+        print('accuracy: %.2f%%' % (100 * accuracy))
